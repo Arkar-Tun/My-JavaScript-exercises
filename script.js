8189f77681a64912af7db1359180822685d496c3
Arkar-Tun (Arkar-Tun) <Arkar-Tun@users.noreply.github.com>
2025-12-28 10:55:39 +0900

    Initialize JavaScript

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-// // 1- Getting User Input!
-let userSelection = prompt("rock, paper, scissors?") .toUpperCase();
+// 1. Select necessary DOM elements
+const cards = document.querySelectorAll(".card"); // Rock, Paper, Scissor ခလုတ်အားလုံး
+const statusText = document.querySelector("#status-text");
+const winnerMsg = document.querySelector("#winner-msg");
 
-// // 2- GEt random number between 0 to 2 for Computer Move! 
-let choices = ["ROCK", "PAPER", "SCISSORS"];
-
-let computerSelection = choices[Math.floor(Math.random() * 3)];
-
-console.log("computer chose: " + computerSelection);
-
-// // 3- Comparison statement for result
-if (userSelection === computerSelection) {
-    console.log("it's a tie!");
-}   else if (
-    (userSelection === "ROCK" && computerSelection === "SCISSORS") ||
-    (userSelection === "PAPER" && computerSelection === "ROCK") ||
-    (userSelection === "SCISSORS" && computerSelection === "PAPER")
-) {
-    console.log("You win!")
-}   else {
-    console.log("You lose!")
+// 2.function that generates a random answer for the computer
+function getComputerMove() {
+  const moves = ["ROCK", "PAPER", "SCISSOR"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return moves[randomNumber];
 }
+
+// 3.checking result function
+function playRound(playerMove, computerMove) {
+  if (playerMove === computerMove) {
+    return "It's a tie!";
+  } else if (
+    (playerMove === "ROCK" && computerMove === "SCISSOR") ||
+    (playerMove === "PAPER" && computerMove === "ROCK") ||
+    (playerMove === "SCISSOR" && computerMove === "PAPER")
+  ) {
+    return "You win! 🎉 ";
+  } else {
+    return "You lose! 💀 )";
+  }
+}
+
+// 4.adding Event Listener to each card
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    // taking result from Player by class name
+    const playerMove = card.classList[0].toUpperCase();
+    const computerMove = getComputerMove();
+
+    // caculating result
+    const result = playRound(playerMove, computerMove);
+
+    // change result in DOM
+    statusText.textContent = result;
+    winnerMsg.textContent = `You chose ${playerMove} | Computer chose ${computerMove}`;
+
+    // change color for win and lose or tie result
+    if (result.includes("win")) {
+      statusText.style.color = "#2ecc71";
+    } else if (result.includes("lose")) {
+      statusText.style.color = "#e74c3c";
+    } else {
+      statusText.style.color = "white";
+    }
+  });
+});
